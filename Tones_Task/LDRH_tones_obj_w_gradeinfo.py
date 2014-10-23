d6bfdb2d923810aaea90ea09291d88ee07f22e8d
Richard <richard.tibbles@gmail.com>
2014-10-23 15:10:47 -0700

    Makes looser constraints on ending practice videos.

--- Tones_Task/LDRH_tones_obj_w_gradeinfo.py
+++ Tones_Task/LDRH_tones_obj_w_gradeinfo.py
@@ -84,7 +84,7 @@ class Tones_Game:
         "Display the instructions for the game."
         #display instructions and wait
         self.audio_inst.play()
-        while self.instructions._player.time <= int(self.instructions.duration*10)/10.0:
+        while self.instructions._player.time <= int(self.instructions.duration):
             self.instructions.draw()
             win.flip()
         win.flip()
