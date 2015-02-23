5fb96ee95947cb9041639aced1be701429382993
Ratnaganadi <chdlab@managua.ucsd.edu>
2015-02-23 12:38:47 -0800

    data analysis change

--- 2data_analysis_phonology.py
+++ 2data_analysis_phonology.py
@@ -5,22 +5,21 @@ import pprint as pp
 import matplotlib.pyplot as plt
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_dir+'/data/complete_data/phonology_old')
-output_name = 'phonology_accuracy_old.xls'
+os.chdir(script_dir+'/data/complete_data/phonology_all')
+output_name = 'phonology_accuracy_combined.xls'
 output_dir = script_dir+'/data_analysis'
 
 #initialize list and variables
 Type = ['threshold','choice']
 data_header = ['Correct Response','Score','Difference Position','Phoneme Difference','Distance']
 games = ['Math','Dots','Reading','Phonology','Spatial','Music']
-x=''; data_byPos={}; data_byPh={}; data_byLevel = {}; data_byDist={}
-tab_header_sum = ['Type','Variable','POA_step','VOT_step','Var_Value','Total_Score','Total_Trial','Accuracy']
-tab_header_byPos = ['Type','Subject_ID','Difference_Position','Total_Score','Total_Trial','Accuracy']
-tab_header_byPh = ['Type','Subject_ID','Phoneme_Pair','POA_step','VOT_step','Total_Score','Total_Trial','Accuracy']
-tab_header_byDist = ['Type','Subject_ID','Distance','Total_Score','Total_Trial','Accuracy']
-tab_header_byLevel = ['Type','Subject_ID','Difficulty_Level','Total_Score','Total_Trial','Accuracy']
-tab_names = ['Summary','Accuracy_byPosition','Accuracy_byPhoneme','Accuracy_byDistance','Accuracy_byLevel']
-
+x=''; data_byPh={}; data_byPhPos={}; data_byResp={}; data_byRespPos={}; data_byPos={}; data_byLevel={}; data_byDist={}
+datalist = [data_byPh,data_byPhPos,data_byPos,data_byLevel,data_byDist]
+# data_byResp,data_byRespPos,
+tab_header_sum = ['Type','Variable','POA_step','VOT_step','Difficulty','Var_Value','Total_Score','Total_Trial','Accuracy']
+tab_header_other = ['Type','Subject_ID','Variable','Var_Value','Total_Score','Total_Trial','Accuracy']
+tab_header_byPh = ['Type','Subject_ID','Variable','Var_Value','POA_step','VOT_step','Total_Score','Total_Trial','Accuracy']
+tab_names = ['Summary','Accuracy_byPhonemePos','Accuracy_byPhoneme','Accuracy_byResp','Accuracy_byRespPos','Accuracy_byPosition','Accuracy_byDistance','Accuracy_byLevel']
 poa_vot = {
     'b-k':[2,1], 'p-g':[2,1], 'k-b':[2,1], 'g-p':[2,1],
     'd-p':[1,1], 't-b':[1,1], 'd-k':[1,1], 'g-t':[1,1],
@@ -34,6 +33,13 @@ difficulty = {
     'dif3_D1_[POA1,VOT0]': ['t-p', 'd-b', 'k-t', 'g-d'],
     'dif4_D2_[POA2,VOT0]_D1_[POA0,VOT1]': ['d-t', 'g-b', 't-d', 'k-p'],
     'dif5_D1_[POA0,VOT1]': ['k-g', 'g-k', 'b-p', 'p-b']
+}
+difno={
+    '1': ['b-k', 'p-g', 'k-b', 'g-p'],
+    '2': ['d-p', 't-b', 'd-k', 'g-t'],
+    '3': ['t-p', 'd-b', 'k-t', 'g-d'],
+    '4': ['d-t', 'g-b', 't-d', 'k-p'],
+    '5': ['k-g', 'g-k', 'b-p', 'p-b']
 }
 
 phoneme_bank = [['b-k','k-b'], ['p-g','g-p'],
@@ -53,10 +59,8 @@ wb = xlwt.Workbook()
 output={}
 for tab in tab_names:
     if tab=='Summary': tab_header = tab_header_sum
-    elif tab=='Accuracy_byPosition': tab_header = tab_header_byPos
-    elif tab=='Accuracy_byPhoneme': tab_header = tab_header_byPh
-    elif tab=='Accuracy_byDistance': tab_header = tab_header_byDist
-    elif tab=='Accuracy_byLevel': tab_header = tab_header_byLevel
+    elif tab=='Accuracy_byPhoneme' or tab=='Accuracy_byPhonemePos': tab_header = tab_header_byPh
+    elif tab=='Accuracy_byPosition': tab_header = tab_header_other
 
     #initialize each tab/sheet and its header
     output[tab] = dict(sheet = wb.add_sheet(tab), headers=tab_header, row=1)
@@ -80,20 +84,31 @@ def write_to_file(tab,outrow,extra_row):
 def calculate_accuracy_then_save(datalist,task_type,subject,var,name):
     for key in datalist.keys():
         templist = [item.astype(np.float64) for item in datalist[key]]
-
         #calculating total score, trial, and accuracy
         total_score = sum(templist)
         total_trial = len(templist)
         accuracy_byVar= float(total_score/total_trial)
-        
-        outrow={}
-        if subject!=None: 
-            if name=='Accuracy_byPhoneme': outrow = {'Type':task_type,'Subject_ID':subject,var:key,'POA_step':poa_vot[key][0],'VOT_step':poa_vot[key][1],'Total_Score':total_score,'Total_Trial':total_trial,'Accuracy':accuracy_byVar}
-            else: outrow = {'Type':task_type,'Subject_ID':subject,var:key,'Total_Score':total_score,'Total_Trial':total_trial,'Accuracy':accuracy_byVar}
-        else: 
-            poa = ''; vot = ''
-            if var=='Phoneme': poa = poa_vot[key][0]; vot = poa_vot[key][1]
-            outrow = {'Type':task_type,'Variable':var,'POA_step':poa,'VOT_step':vot,'Var_Value':key,'Total_Score':total_score,'Total_Trial':total_trial,'Accuracy':accuracy_byVar}
+        outrow={'Type':task_type,'Variable':var,'Var_Value':key,'Total_Score':total_score,'Total_Trial':total_trial,'Accuracy':accuracy_byVar}
+
+        poa = ''; vot = ''; diff = ''
+        if name=='Accuracy_byPhoneme' or var=='Phoneme': 
+            poa = poa_vot[key][0]
+            vot = poa_vot[key][1]
+            for difkey in difno.keys():
+                if key in difno[difkey]: diff = difkey;
+        elif name=='Accuracy_byPhonemePos' or var=='PhonemePos':
+            ph = key.split('_')[0]
+            poa = poa_vot[ph][0]
+            vot = poa_vot[ph][1]
+            for difkey in difno.keys():
+                if ph in difno[difkey]: diff = difkey
+        if subject!=None: outrow['Subject_ID']=subject
+        if (subject!=None and poa!='' and vot!='') or subject==None:
+            outrow['POA_step']=poa
+            outrow['VOT_step']=vot
+            outrow['Difficulty']=diff
+
+        # 'POA_step':poa,'VOT_step':vot,'Var_Value':key,'Total_Score':total_score,'Total_Trial':total_trial,'Accuracy':accuracy_byVar}
         write_to_file(name,outrow,'no_extra_row')
 
 
@@ -110,25 +125,23 @@ for filename in filenames:
     for i in range(0,len(data_header)): rawdata[i+1] = list(sheet[data_header[i]])
 
 
-
     #initializing dictionaries and lists
-    data_byPos[subject_id]={}
-    data_byPh[subject_id]={}
-    data_byLevel[subject_id]={}
-    data_byDist[subject_id]={}
+    for data in datalist: data[subject_id] = {}
+
     for x in ['threshold','choice']:
-        data_byPos[subject_id][x]={}
-        data_byPh[subject_id][x]={}
-        data_byLevel[subject_id][x]={}
-        data_byDist[subject_id][x]={}
+        for data in datalist: data[subject_id][x]={}
+
         for i in range(0,len(rawdata[0])):
             data_byPos[subject_id][x][rawdata[3][i]] = []
-            # data_byPh[subject_id][x][rawdata[4][i]] = []
             data_byDist[subject_id][x][rawdata[5][i]] = []
-            
+            # data_byResp[subject_id][x][rawdata[0][i]] = []
+            # data_byRespPos[subject_id][x][rawdata[0][i]+'_'+str(int(rawdata[3][i]))] = []
+
             for pair in phoneme_bank:
                 if rawdata[4][i] in pair and rawdata[4][i] not in data_byPh[subject_id][x].keys():
                     data_byPh[subject_id][x][pair[0]]=[]
+                    for pos in [1,2,3]: data_byPhPos[subject_id][x][pair[0]+'_'+str(pos)]=[]
+
         for dif in difficulty.keys():
             data_byLevel[subject_id][x][dif]=[]
 
@@ -136,76 +149,82 @@ for filename in filenames:
     for i in range(0,len(rawdata[0])):
         if i==0 and (rawdata[0][i+1]-rawdata[0][i])==1: data_type='threshold'
         elif i!=0 and (rawdata[0][i]-rawdata[0][i-1])==1: data_type='threshold'
-        # elif i!=0 and (rawdata[0][i]-rawdata[0][i-1])>1: data_type='choice'
         else: data_type='choice'
 
         #reorganize raw data into prepared dictionaries/lists
         if rawdata[1][i]=='different' and pd.isnull(rawdata[3][i])!=True:
             data_byPos[subject_id][data_type][rawdata[3][i]].append(rawdata[2][i]) #datalist['threshold'][position]=score
-            # data_byPh[subject_id][data_type][rawdata[4][i]].append(rawdata[2][i]) #datalist['threshold'][phoneme_pair]=score
             data_byDist[subject_id][data_type][rawdata[5][i]].append(rawdata[2][i]) #datalist['threshold'][distance]=score
+            # data_byResp[subject_id][x][rawdata[0][i]].append(rawdata[2][i])
+            # data_byRespPos[subject_id][x][rawdata[0][i]+'_'+str(int(rawdata[3][i]))].append(rawdata[2][i])
 
             #put score data of the same phoneme pairs in the same list
             for pair in phoneme_bank:
-                if rawdata[4][i] in pair: data_byPh[subject_id][data_type][pair[0]].append(rawdata[2][i])
+                if rawdata[4][i] in pair: 
+                    data_byPh[subject_id][data_type][pair[0]].append(rawdata[2][i])
+                    data_byPhPos[subject_id][data_type][pair[0]+'_'+str(int(rawdata[3][i]))].append(rawdata[2][i])
+                    # if subject_id=='P0204': print 'test',subject_id,pair[0]+'_'+str(int(rawdata[3][i]))
             #organize score data based on level of difficulty only
             for dif in difficulty.keys():
                 if rawdata[4][i] in difficulty[dif]:
                     data_byLevel[subject_id][data_type][dif].append(rawdata[2][i]) #datalist['threshold'][difficulty_level]=score
 
-
     #removing empty dictionaries:
     for x in Type:
-        data_byPos[subject_id][x] = dict((k,v) for k,v in data_byPos[subject_id][x].items() if v)
-        data_byPh[subject_id][x] = dict((k,v) for k,v in data_byPh[subject_id][x].items() if v)
-        data_byLevel[subject_id][x] = dict((k,v) for k,v in data_byLevel[subject_id][x].items() if v)
-        data_byDist[subject_id][x] = dict((k,v) for k,v in data_byDist[subject_id][x].items() if v)
+        for data in datalist: data[subject_id][x] = dict((k,v) for k,v in data[subject_id][x].items() if v)
     
         # x='threshold'
         #calculate accuracy by position, phoneme_pair and distance
-        calculate_accuracy_then_save(data_byPos[subject_id][x],x,subject_id,'Difference_Position','Accuracy_byPosition')
         calculate_accuracy_then_save(data_byPh[subject_id][x],x,subject_id,'Phoneme_Pair','Accuracy_byPhoneme')
+        calculate_accuracy_then_save(data_byPhPos[subject_id][x],x,subject_id,'Phoneme_Pair','Accuracy_byPhonemePos')
+        # calculate_accuracy_then_save(data_byResp[subject_id][x],x,subject_id,'Correct_Resp','Accuracy_byPhoneme')
+        # calculate_accuracy_then_save(data_byRespPos[subject_id][x],x,subject_id,'Correct_Resp','Accuracy_byPhonemePos')
+        calculate_accuracy_then_save(data_byPos[subject_id][x],x,subject_id,'Difference_Position','Accuracy_byPosition')
         calculate_accuracy_then_save(data_byDist[subject_id][x],x,subject_id,'Distance','Accuracy_byDistance')
         calculate_accuracy_then_save(data_byLevel[subject_id][x],x,subject_id,'Difficulty_Level','Accuracy_byLevel')
 
     #adding extra row at the end of each participant's data
-    write_to_file('Accuracy_byPosition',{},'extra_row')
     write_to_file('Accuracy_byPhoneme',{},'extra_row')
+    write_to_file('Accuracy_byPhonemePos',{},'extra_row')
+    write_to_file('Accuracy_byPosition',{},'extra_row')
     write_to_file('Accuracy_byDistance',{},'extra_row')
     write_to_file('Accuracy_byLevel',{},'extra_row')
 
-
+# print 'databylevel',data_byLevel
 ## data processing ## calculating values per game/task across all subject ID
 # print 'data_byPh', data_byPh
 
 y='threshold'
 #initializing dictionaries and lists
-sum_byPos = {}; sum_byPh = {}; sum_byDist = {}; sum_byPOA_VOT = {}; sum_byLevel = {}
-sum_byPos[y] = {}; sum_byPh[y] = {}; sum_byDist[y] = {}; sum_byPOA_VOT[y] = {}; sum_byLevel[y] = {}
+sum_byPh = {}; sum_byPhPos = {}; sum_byPos = {}; sum_byDist = {}; sum_byPOA_VOT = {}; sum_byLevel = {}
+sum_byPh[y] = {}; sum_byPhPos[y] = {}; sum_byPos[y] = {}; sum_byDist[y] = {}; sum_byPOA_VOT[y] = {}; sum_byLevel[y] = {}
 for filename in filenames:
     subject_id = filename.split('_')[0]
-    for dif in data_byPos[subject_id][y].keys(): sum_byPos[y][dif]=[]
-    for dif in data_byPh[subject_id][y].keys(): 
-        sum_byPh[y][dif]=[]
-        category = 'POA'+str(poa_vot[dif][0])+'_VOT'+str(poa_vot[dif][1])
-        sum_byPOA_VOT[y][category]=[]
-    for dif in data_byDist[subject_id][y].keys(): sum_byDist[y][dif]=[]
-    for dif in data_byLevel[subject_id][y].keys(): sum_byLevel[y][dif]=[]
+    datalist1 = [data_byPos[subject_id][y], data_byPh[subject_id][y], data_byPhPos[subject_id][y], data_byDist[subject_id][y], data_byLevel[subject_id][y]]
+    sumlist = [sum_byPos[y], sum_byPh[y], sum_byPhPos[y], sum_byDist[y], sum_byLevel[y]]
+
+    for i in range(0,5):
+        for dif in datalist1[i].keys(): 
+            sumlist[i][dif]=[]
+            if i==1:
+                category = 'POA'+str(poa_vot[dif][0])+'_VOT'+str(poa_vot[dif][1])
+                sum_byPOA_VOT[y][category]=[]
 
 for filename in filenames:
     subject_id = filename.split('_')[0]
-    for dif in data_byPos[subject_id][y].keys(): sum_byPos[y][dif].extend(data_byPos[subject_id][y][dif])
-    for dif in data_byPh[subject_id][y].keys(): 
-        sum_byPh[y][dif].extend(data_byPh[subject_id][y][dif])
-        category = 'POA'+str(poa_vot[dif][0])+'_VOT'+str(poa_vot[dif][1])
-        sum_byPOA_VOT[y][category].extend(data_byPh[subject_id][y][dif])
-
-    for dif in data_byDist[subject_id][y].keys(): sum_byDist[y][dif].extend(data_byDist[subject_id][y][dif])
-    for dif in data_byLevel[subject_id][y].keys(): sum_byLevel[y][dif].extend(data_byLevel[subject_id][y][dif])
+    datalist1 = [data_byPos[subject_id][y], data_byPh[subject_id][y], data_byPhPos[subject_id][y], data_byDist[subject_id][y], data_byLevel[subject_id][y]]
+    sumlist = [sum_byPos[y], sum_byPh[y], sum_byPhPos[y], sum_byDist[y], sum_byLevel[y]]
+    for i in range(0,5):
+        for dif in datalist1[i].keys(): 
+            sumlist[i][dif].extend(datalist1[i][dif])
+            if i==1:
+                category = 'POA'+str(poa_vot[dif][0])+'_VOT'+str(poa_vot[dif][1])
+                sum_byPOA_VOT[y][category].extend(datalist1[i][dif])
 
 ##calculate mean accuracy per difficulty level by variables + add extra line after each variable
-calculate_accuracy_then_save(sum_byPos[y],y,None,'Position','Summary'); write_to_file('Summary',{},'extra_row')
 calculate_accuracy_then_save(sum_byPh[y],y,None,'Phoneme','Summary'); write_to_file('Summary',{},'extra_row')
+calculate_accuracy_then_save(sum_byPhPos[y],y,None,'PhonemePos','Summary'); write_to_file('Summary',{},'extra_row')
+calculate_accuracy_then_save(sum_byPos[y],y,None,'Position','Summary'); write_to_file('Summary',{},'extra_row')
 calculate_accuracy_then_save(sum_byPOA_VOT[y],y,None,'POA and VOT','Summary'); write_to_file('Summary',{},'extra_row')
 calculate_accuracy_then_save(sum_byDist[y],y,None,'Distance','Summary'); write_to_file('Summary',{},'extra_row')
 calculate_accuracy_then_save(sum_byLevel[y],y,None,'Difficulty_Level','Summary'); write_to_file('Summary',{},'extra_row')
