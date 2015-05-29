b9ac2c2b7efde735574aef5c51b9b37222b0dece
Richard Tibbles <richard.tibbles@gmail.com>
2015-05-29 10:46:06 -0700

    Move deploy.py back into root dir.

--- deploy.py
+++ deploy.py
@@ -1,8 +1,4 @@
 from subprocess import check_output
-from dir_function import get_homepath
-
-dir_hierarchy = 1
-homepath = get_homepath(dir_hierarchy,go_home='')
 
 commit_hash = check_output(["git","rev-parse","HEAD"])
 
